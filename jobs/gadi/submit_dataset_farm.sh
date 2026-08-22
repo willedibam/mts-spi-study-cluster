ddbf2eea4b6b766627c62912d9b057ad751373c8
Will Edibam <willedibam@icloud.com>
2026-08-22 10:52:34 +1000

    Add bounded Gadi task execution

--- jobs/gadi/submit_dataset_farm.sh
+++ jobs/gadi/submit_dataset_farm.sh
@@ -26,9 +26,15 @@ walltime="${WALLTIME:-01:00:00}"
 jobfs_gb="${JOBFS_GB:-10}"
 pyspi_config="${PYSPI_CONFIG:-configs/pyspi/benchmarked_p90.yaml}"
 test -f "$pyspi_config"
+task_timeout="${TASK_TIMEOUT:-}"
 
-echo "[INFO] config=$config datasets=$start..$end/$total ncpus=$ncpus mem=${mem_gb}GB walltime=$walltime" >&2
+variables="EXPERIMENT_CONFIG=$config,START_INDEX=$start,END_INDEX=$end,PYSPI_CONFIG=$pyspi_config"
+if [[ -n "$task_timeout" ]]; then
+    variables+=",TASK_TIMEOUT=$task_timeout"
+fi
+
+echo "[INFO] config=$config datasets=$start..$end/$total ncpus=$ncpus mem=${mem_gb}GB walltime=$walltime task_timeout=${task_timeout:-none}" >&2
 qsub \
     -l "ncpus=$ncpus,mem=${mem_gb}GB,walltime=$walltime,jobfs=${jobfs_gb}GB" \
-    -v "EXPERIMENT_CONFIG=$config,START_INDEX=$start,END_INDEX=$end,PYSPI_CONFIG=$pyspi_config" \
+    -v "$variables" \
     jobs/gadi/run_dataset_farm.pbs
